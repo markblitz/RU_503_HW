427d74248920419dc74bbd3d8d7c9c2e46f5aad6
markblitz <blitzblq@gmail.com>
2019-10-03 11:39:27 -0400

    more cases finished

--- Homework_4/Book_list.cpp
+++ Homework_4/Book_list.cpp
@@ -8,22 +8,27 @@
 using namespace std;
 
 bool Full_Check(const vector<int>& list);
+bool Empty_Check(const vector<int>& list);
 void Insert_End(vector<int>& list, int new_book);
-void Insert_Position(vector<int>& list, int new_book, int position);
+void Insert_Position(vector<int>& list, int new_book, unsigned int position);
+int Linear_Search(const vector<int>& list, int aim_ISBN);
 
+void Delete_Position(vector<int>& list, unsigned int position);
+
+void Remove_Space(vector<int>& list);
 void Print_List(const vector<int>& list);
 
 int main() {
 	// initialization
 	int menu_user_choice = 0;
-	vector<int> book_list(5, 0);
+	vector<int> book_list(7, 0);
 
 	// ask user choice in huge loop
 	while (true) {
 		cout << "Welcome to the Book list program.\n" << "What would you like to do?\n";
 		cout << "	1. add an element to the end of list\n" << "	2. edd an element at a loction\n";
-	//	cout << "	3. find an element by ISBN number (linear search)\n" << "	4. fina an element by ISBN number (binary search)\n";
-	//	cout << "	5. delete an element at position\n" << "	6. delete an element by ISBN number\n";
+		cout << "	3. find an element by ISBN number (linear search)\n" << "	4. fina an element by ISBN number (binary search)\n";
+		cout << "	5. delete an element at position\n" << "	6. delete an element by ISBN number\n";
 	//	cout << "	7. sort the list (using selection sort)\n" << "	8. sort the list (using bubble sort)\n";
 		cout << "	9. print the list\n" << "	10. exit\n" << "your choice ---\n";
 
@@ -58,13 +63,32 @@ int main() {
 
 			cout << "Please enter new book ISBN and position to insert at the position:\n";
 			int new_book_ISBN = 0;
-			int insert_position = 0;
-
+			unsigned int insert_position = 0;
+			cin >> new_book_ISBN >> insert_position;
+			// position check, avoid too big or negative position
+			if ((insert_position < 0) || (insert_position > book_list.size())) {
+				cout << "Invaild position!\n";
+				continue;
+			}
+			Insert_Position(book_list, new_book_ISBN, insert_position);
+			Remove_Space(book_list);
+			Print_List(book_list);
 			continue;
 		}
 
 		case 3:
-			cout << "input " << menu_user_choice << endl;
+		{
+			cout << "Please enter the ISBN you want to find:\n";
+			int aim_ISBN = 0;
+			cin >> aim_ISBN;
+			int position = 0;
+			position = Linear_Search(book_list, aim_ISBN);
+			if (position == 0) {
+				cout << "The ISBN number may not be in the list!\n\n";
+				continue;
+			}
+			cout << "The ISBN number " << aim_ISBN << " you are looking for is at position " << position << endl << endl;
+		}
 			continue;
 
 		case 4:
@@ -72,11 +96,51 @@ int main() {
 			continue;
 
 		case 5:
-			cout << "input " << menu_user_choice << endl;
+		{
+			bool list_empty = 0;
+			list_empty = Empty_Check(book_list);
+			if (list_empty == 1) {
+				cout << "The list is empty! Try insert first!\n\n";
+				continue;
+			}
+
+			cout << "Please enter the position to delete at the position:\n";
+			unsigned int delete_position = 0;
+			cin >> delete_position;
+			// position check, avoid too big or negative position
+			if ((delete_position < 0) || (delete_position > book_list.size())) {
+				cout << "Invaild position!\n";
+				continue;
+			}
+			Delete_Position(book_list, delete_position);
+			Remove_Space(book_list);
+			Print_List(book_list);
+			continue;
+		}
 			continue;
 
 		case 6:
-			cout << "input " << menu_user_choice << endl;
+		{
+			bool list_empty = 0;
+			list_empty = Empty_Check(book_list);
+			if (list_empty == 1) {
+				cout << "The list is empty! Try insert first!\n\n";
+				continue;
+			}
+
+			cout << "Please enter the ISBN you want to delete:\n";
+			int aim_ISBN = 0;
+			cin >> aim_ISBN;
+			int position = 0;
+			position = Linear_Search(book_list, aim_ISBN);
+			if (position == 0) {
+				cout << "The ISBN number may not be in the list!\n\n";
+				continue;
+			}
+			Delete_Position(book_list, position);
+			Remove_Space(book_list);
+			Print_List(book_list);
+		}
 			continue;
 
 		case 7:
@@ -108,15 +172,33 @@ int main() {
 
 bool Full_Check(const vector<int>& list)
 {
-	const vector<int>* book_list_pointer;
-	book_list_pointer = &list;
+	const vector<int>* list_pointer;
+	list_pointer = &list;
 	size_t i = 0;
 	while (true) {
-		//cout << i << "  " << book_list_pointer->size() << endl;
-		if (i == book_list_pointer->size()) {			
+		//cout << i << "  " << list_pointer->size() << endl;
+		if (i == list_pointer->size()) {			
 			return true;
 		}
-		if (book_list_pointer->at(i) != 0) {
+		if (list_pointer->at(i) != 0) {
+			i++;
+			continue;
+		}
+		return false;
+	}
+}
+
+bool Empty_Check(const vector<int>& list)
+{
+	const vector<int>* list_pointer;
+	list_pointer = &list;
+	size_t i = 0;
+	while (true) {
+		//cout << i << "  " << list_pointer->size() << endl;
+		if (i == list_pointer->size()) {
+			return true;
+		}
+		if (list_pointer->at(i) == 0) {
 			i++;
 			continue;
 		}
@@ -126,34 +208,78 @@ bool Full_Check(const vector<int>& list)
 
 void Insert_End(vector<int>& list, int new_book)
 {
-	vector<int>* book_list_pointer;
-	book_list_pointer = &list;
-	for (size_t i = 0; i <= book_list_pointer->size(); i++) {
-		if (book_list_pointer->at(i) == 0) {
-			book_list_pointer->at(i) = new_book;
+	vector<int>* list_pointer;
+	list_pointer = &list;
+	for (size_t i = 0; i <= list_pointer->size(); i++) {
+		if (list_pointer->at(i) == 0) {
+			list_pointer->at(i) = new_book;
 			break;
 		}	
 	}
 }
 
-void Insert_Position(vector<int>& list, int new_book, int position)
+void Insert_Position(vector<int>& list, int new_book, unsigned int position)
 {
-	vector<int>* book_list_pointer;
-	book_list_pointer = &list;
-	book_list_pointer->at(position - 1) = new_book;
+	vector<int>* list_pointer;
+	list_pointer = &list;
+	if (list_pointer->at(position - 1) != 0) {
+		for (size_t i = list_pointer->size() - 1; i > position - 1; i--) {
+			list_pointer->at(i) = list_pointer->at(i - 1);
+		}
+	}
+	list_pointer->at(position - 1) = new_book;
+}
+
+int Linear_Search(const vector<int>& list, int aim_ISBN)
+{
+	const vector<int>* list_pointer;
+	list_pointer = &list;
+	for (size_t i = 0; i < list_pointer->size(); i++) {
+		if (list_pointer->at(i) == aim_ISBN) {
+			return i + 1;
+		}
+	}
+	return 0;
+}
+
+void Delete_Position(vector<int>& list, unsigned int position)
+{
+	vector<int>* list_pointer;
+	list_pointer = &list;
+	if (list_pointer->at(position - 1) == 0) {
+		cout << "The position is already empty!\n";
+	}
+	list_pointer->at(position - 1) = 0;
+}
+
+void Remove_Space(vector<int>& list)
+{
+	vector<int>* list_pointer;
+	list_pointer = &list;
+	for (size_t i = 0; i < list_pointer->size(); i++) {
+		if (list_pointer->at(i) == 0) {
+			for (size_t j = i; j < list_pointer->size() - 1; j++) {
+				if (list_pointer->at(j + 1) != 0) {
+					list_pointer->at(i) = list_pointer->at(j + 1);
+					list_pointer->at(j + 1) = 0;
+					break;
+				}
+			}
+		}
+	}
 }
 
 void Print_List(const vector<int>& list)
 {
 	cout << "The current book list is:\n";
-	const vector<int>* book_list_pointer;
-	book_list_pointer = &list;
-	for (size_t i = 0; i < book_list_pointer->size(); i++) {
-		if (book_list_pointer->at(i) != 0) {
-			cout << setw(3) << i + 1 << ". " << book_list_pointer->at(i) << endl;
+	const vector<int>* list_pointer;
+	list_pointer = &list;
+	for (size_t i = 0; i < list_pointer->size(); i++) {
+		//if (list_pointer->at(i) != 0) {
+			cout << setw(3) << i + 1 << ". " << list_pointer->at(i) << endl;
 			continue;
-		}
-		break;
+		//}
+		//break;
 	}
 	cout << "Book list ends here!\n" << endl;
 }
